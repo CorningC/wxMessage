edc064ade1f8b2782e76691d1fad25530e71df2a
CorningC <113528933+CorningC@users.noreply.github.com>
2022-09-16 11:46:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import http.client, urllib
 import json
 from zhdate import ZhDate
 def get_color():
-    # 获取随机颜色
+    # 获取随机颜 色
     get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
     color_list = get_colors(100)
     return random.choice(color_list)
